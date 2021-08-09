594208a28d0901634f763103a579eb0d4cf4e7d3
Simone Westphal <hello@swestphal.io>
2021-08-09 13:57:01 +0200

    refactor: Add main-navigation bar

---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Header = () => {
+    return (
+        <div className="ui secondary pointing menu">
+            <Link to="/" className="item">
+                Streamy
+            </Link>
+            <div className="right menu">
+                <Link to="/streams/new" className="item">
+                    new
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
